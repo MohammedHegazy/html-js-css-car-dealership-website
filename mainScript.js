fcc45454022231c000bce64ce797029d06869d31
Null-Edge <nulledge.it@gmail.com>
2020-12-25 15:11:07 +0200

    style changes
    خلفية + تعديل على كلاس الصور + كلاس النص

--- mainScript.js
+++ mainScript.js
@@ -11,15 +11,15 @@ for (var i = 0; i < menuButton.length; i++)
     //set onmouseout and mouseover for each button
     menuButton[i].setAttribute("onmouseout","mouseOutButton(this)");
     menuButton[i].setAttribute("onmouseover","mouseOverButton(this, "+red+","+green+","+blue+");");
-    menuButton[i].style.backgroundColor = "grey";
+    menuButton[i].style.backgroundColor = "#2D1E2F";
 }
 
 function mouseOverButton(button, red,green,blue)
 {
     button.style.backgroundColor = 'rgba('+red+','+green+','+blue+',0.5)';
 }
-
+//55DDE0
 function mouseOutButton(button) 
 {
-    button.style.backgroundColor = "grey";
+    button.style.backgroundColor = "#2D1E2F";
 }
