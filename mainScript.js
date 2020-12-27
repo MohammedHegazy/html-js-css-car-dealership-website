381ba64753fdf8ed6d5a466361395740baa63e39
Null-Edge <nulledge.it@gmail.com>
2020-12-27 19:22:21 +0200

    Auto stash before merge of "main" and "origin/main"

--- mainScript.js
+++ mainScript.js
@@ -1,25 +1,18 @@
 menuButton = document.getElementsByName("menuButton");
 
 for (var i = 0; i < menuButton.length; i++)
-{//loop through all buttons
-
-    //get a random color
-    var red = parseInt(Math.random() * 255 +1);
-    var green = parseInt(Math.random() * 255 +1);
-    var blue = parseInt(Math.random() * 255 +1);
-    
+{   //loop through all buttons
     //set onmouseout and mouseover for each button
-    menuButton[i].setAttribute("onmouseout","mouseOutButton(this)");
-    menuButton[i].setAttribute("onmouseover","mouseOverButton(this, "+red+","+green+","+blue+");");
-    menuButton[i].style.backgroundColor = "#2D1E2F";
+    menuButton[i].setAttribute("onmouseout","mouseOutButton(this);");
+    menuButton[i].setAttribute("onmouseover","mouseOverButton(this);");
 }
 
-function mouseOverButton(button, red,green,blue)
+function mouseOutButton(button) 
 {
     button.style.backgroundColor = 'rgba('+red+','+green+','+blue+',0.5)';
 }
 //55DDE0
 function mouseOutButton(button) 
 {
-    button.style.backgroundColor = "#2D1E2F";
+    button.style.backgroundColor = "#3c6e71";
 }
