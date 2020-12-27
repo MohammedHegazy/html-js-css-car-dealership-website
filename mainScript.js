6e05f8580d9ae837c63a6114b035c6a252366dba
Null-Edge <nulledge.it@gmail.com>
2020-12-27 20:41:54 +0200

    Home page major
    تعديل شكل القوائم كامل مع اضافة اخفاء و ظهور عند السكرول
    تعديل الروابط و الصورة الخلفية  و سيستم الالوان

--- mainScript.js
+++ mainScript.js
@@ -7,12 +7,35 @@ for (var i = 0; i < menuButton.length; i++)
     menuButton[i].setAttribute("onmouseover","mouseOverButton(this);");
 }
 
-function mouseOutButton(button) 
+function mouseOverButton(button) 
 {
-    button.style.backgroundColor = 'rgba('+red+','+green+','+blue+',0.5)';
+    button.style.backgroundColor = '#4DFFF3';
+    button.style.color  = "#503D3F";
 }
 //55DDE0
 function mouseOutButton(button) 
 {
-    button.style.backgroundColor = "#3c6e71";
+    button.style.backgroundColor = "#503D3F";
+    button.style.color  = '#4DFFF3';
 }
+
+/* if scrolled to bottom show bottom*/
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) 
+    {
+        document.getElementById("contactus").style.visibility = 'visible';
+    }
+    else if($(window).scrollTop() + $(window).height() < $(document).height() - 100) 
+    {
+        document.getElementById("contactus").style.visibility = 'hidden';
+    }
+    
+    if($(window).scrollTop() < 120)
+    {
+        document.getElementById("mainMenu").style.visibility = 'visible';
+    }
+    else
+    {
+        document.getElementById("mainMenu").style.visibility = 'hidden';    
+    }
+  });
